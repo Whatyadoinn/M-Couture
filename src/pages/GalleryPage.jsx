cbc6c4c8c40c781d5a8596a26f415a0c1f33ace5
Whatyadoinn <minehome205@gmail.com>
2026-08-11 11:03:40 +0530

    n

--- src/pages/GalleryPage.jsx
+++ src/pages/GalleryPage.jsx
@@ -1,7 +1,6 @@
 import PageBanner from "../components/PageBanner";
-import BestsellersCarousel from "../components/BestsellersCarousel";
 import OurClients from "../components/OurClients";
-import GoogleReviews from "../components/GoogleReviews";
+import ClientReviews from "../components/GoogleReviews";
 import ExhibitionVenues from "../components/ExhibitionVenues";
 
 export default function GalleryPage() {
@@ -10,13 +9,12 @@ export default function GalleryPage() {
       <PageBanner
         eyebrow="The M'Couture Experience"
         title="Gallery & Venues"
-        description="Discover our bestsellers, client stories, and upcoming showcases."
+        description="Discover our client stories and upcoming showcases."
         image="https://images.unsplash.com/photo-1595777457583-95e059d581b8?q=100&w=3840&auto=format&fit=crop"
       />
-      <BestsellersCarousel />
       <OurClients />
+      <ClientReviews />
       <ExhibitionVenues />
-      <GoogleReviews />
     </>
   );
 }
